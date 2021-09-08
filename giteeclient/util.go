0d6627218b3158fd44112e0f1d96fe74cd052043
zengchen1024 <chenzeng2@huawei.com>
2021-09-08 19:27:59 +0800

    parse pr action

--- giteeclient/util.go
+++ giteeclient/util.go
@@ -1,6 +1,8 @@
 package giteeclient
 
 import (
+	"strings"
+
 	sdk "gitee.com/openeuler/go-gitee/gitee"
 	"k8s.io/apimachinery/pkg/util/sets"
 )
@@ -49,4 +51,36 @@ func GetOwnerAndRepoByPREvent(pre *sdk.PullRequestEvent) (string, string) {
 //GetOwnerAndRepoByIssueEvent obtain the owner and repository name from the issue's event
 func GetOwnerAndRepoByIssueEvent(issue *sdk.IssueEvent) (string, string) {
 	return issue.Repository.Namespace, issue.Repository.Path
+}
+
+const (
+	PRActionOpened              = "opened"
+	PRActionClosed              = "closed"
+	PRActionUpdatedLabel        = "update_label"
+	PRActionChangedTargetBranch = "target_branch_changed"
+	PRActionChangedSourceBranch = "source_branch_changed"
+)
+
+func GetPullRequestAction(e *sdk.PullRequestEvent) string {
+	switch strings.ToLower(*(e.Action)) {
+	case "open":
+		return PRActionOpened
+
+	case "update":
+		switch strings.ToLower(*(e.ActionDesc)) {
+		case "source_branch_changed": // change the pr's commits
+			return PRActionChangedSourceBranch
+
+		case "target_branch_changed": // change the branch to which this pr will be merged
+			return PRActionChangedTargetBranch
+
+		case "update_label":
+			return PRActionUpdatedLabel
+		}
+
+	case "close":
+		return PRActionClosed
+	}
+
+	return ""
 }
