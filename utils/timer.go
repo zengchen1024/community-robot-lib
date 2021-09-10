8ba84b6fda84a38b25b5ef69f9452f47cc3a3d7e
zengchen1024 <chenzeng2@huawei.com>
2021-09-10 17:58:16 +0800

    fix exit timer safely

--- utils/timer.go
+++ utils/timer.go
@@ -3,20 +3,21 @@ package utils
 import "time"
 
 type Timer struct {
-	stop chan bool
+	stop   chan bool
+	stoped bool
 }
 
 // Start starts work. If the first attempt fails, then returns the error.
 // It will trigger the work by the interval until recieving a stop signal.
-func (t Timer) Start(f func(), interval time.Duration) {
+func (t *Timer) Start(f func(), interval time.Duration) {
 	ticker := time.Tick(interval)
 	go func() {
 		for {
-			//TODO is it right
 			select {
 			case <-ticker:
 				f()
 			case <-t.stop:
+				t.stoped = true
 				break
 			}
 		}
@@ -25,4 +26,11 @@ func (t Timer) Start(f func(), interval time.Duration) {
 
 func (t *Timer) Stop() {
 	t.stop <- true
+
+	ticker := time.Tick(1 * time.Millisecond)
+	for range ticker {
+		if t.stoped {
+			break
+		}
+	}
 }
