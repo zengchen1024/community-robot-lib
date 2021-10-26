b75595537c9f3fc961bcfe1ec75a270e56950250
zengchen1024 <chenzeng2@huawei.com>
2021-10-26 17:00:04 +0800

    update shell scripts

--- new-plugin/publish/command_status.sh
+++ new-plugin/publish/command_status.sh
@@ -12,10 +12,15 @@ build_date=$(date -u '+%Y%m%d')
 image_tag="v${build_date}-${commit_id}"
 repository=$(pwd | xargs dirname | xargs basename)
 
+image_registry=${IMAGE_REGISTRY_OVERRIDE:-swr.cn-north-4.myhuaweicloud.com}
+image_repo=${IMAGE_REPO_OVERRIDE:-opensourceway/robot/$repository}
+image_tag=${IMAGE_TAG_OVERRIDE:-$image_tag}
+
 cat <<EOF
-IMAGE_REGISTRY ${IMAGE_REGISTRY_OVERRIDE:-swr.ap-southeast-1.myhuaweicloud.com}
-IMAGE_REPO ${IMAGE_REPO_OVERRIDE:-opensourceway/robot/$repository}
-IMAGE_TAG ${IMAGE_TAG_OVERRIDE:-$image_tag}
+IMAGE_REGISTRY ${image_registry}
+IMAGE_REPO ${image_repo}
+IMAGE_TAG ${image_tag}
+IMAGE_ID ${image_registry}/${image_repo}:${image_tag}
 EOF
 
 cd $work_dir
