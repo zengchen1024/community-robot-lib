05cfdcd35d6e4cfa3cc8f284860098eddd222815
zengchen1024 <chenzeng2@huawei.com>
2021-11-02 14:35:18 +0800

    update command_status.sh

--- new-plugin/publish/command_status.sh
+++ new-plugin/publish/command_status.sh
@@ -7,20 +7,30 @@ set -o pipefail
 work_dir=$(pwd)
 cd $(dirname $0)
 
-branch=$(git symbolic-ref --short HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
+branch=${BRANCH_OVERRIDE:-$branch}
+
 commit_id=$(git describe --tags --always --dirty)
+commit_id=${COMMIT_ID_OVERRIDE:-$commit_id}
+
+repository=$(git remote -v | tail -1 | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')
+if [ -z "$repository" ]; then
+    repository=$(pwd | xargs dirname | xargs basename)
+fi
+repository=${REPOSITORY_OVERRIDE:-$repository}
+
 image_tag="${branch}-${commit_id}"
-repository=$(pwd | xargs dirname | xargs basename)
+image_tag=${IMAGE_TAG_OVERRIDE:-$image_tag}
 
 image_registry=${IMAGE_REGISTRY_OVERRIDE:-swr.cn-north-4.myhuaweicloud.com}
 image_repo=${IMAGE_REPO_OVERRIDE:-opensourceway/robot/$repository}
-image_tag=${IMAGE_TAG_OVERRIDE:-$image_tag}
 
 cat <<EOF
 IMAGE_REGISTRY ${image_registry}
 IMAGE_REPO ${image_repo}
 IMAGE_TAG ${image_tag}
 IMAGE_ID ${image_registry}/${image_repo}:${image_tag}
+CODE_REPOSITORY ${repository}
 EOF
 
 cd $work_dir
