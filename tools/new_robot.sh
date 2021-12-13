7e9ae82b43684e3ef32a25fbd2d2a7575ff36836
zengchen <chenzeng2@huawei.com>
2021-12-13 16:06:14 +0800

    Update new plugin (#44)
    * update new plugin script
    
    * upate the usage of new_robot.sh

--- tools/new_robot.sh
+++ tools/new_robot.sh
@@ -14,37 +14,42 @@ underscore_to_hyphen(){
     echo ${name//_/-}
 }
 
+log(){
+    local s=$1
+    echo -e "\n${s}\n"
+}
+
 check_robot_name() {
     repo_name=$1
     local name=""
 
     name=$(echo "$repo_name" | awk '{print tolower($0)}')
     if [ "$name" != "$repo_name" ]; then
-        echo "Info: the robot name($repo_name) includes uppercase characters, and will be changed to $name."
+        log "Info: the robot name($repo_name) includes uppercase characters, and will be changed to $name."
         repo_name=$name
     fi
 
     name=$(underscore_to_hyphen $repo_name)
     if [ "$name" != "$repo_name" ]; then
-        echo "Info: the robot name($repo_name) includes '_', and will be changed to $name."
+        log "Info: the robot name($repo_name) includes '_', and will be changed to $name."
         repo_name=$name
     fi
 
     name=$(echo "$repo_name" | sed -e 's/[a-z0-9-]//g')
     if [ -n "$name" ]; then
-        echo "Error: the robot name should only include characters of letter(a-z), digitals and '-'"
+        log "Error: the robot name should only include characters of letter(a-z), digitals and '-'"
         return 1
     fi
 
     name=$(echo "$repo_name" | sed -e 's/--*/-/g')
     if [ "$name" != "$repo_name" ]; then
-        echo "Info: the robot name($repo_name) includes multiple '-', and will be changed to $name."
+        log "Info: the robot name($repo_name) includes multiple '-', and will be changed to $name."
         repo_name=$name
     fi
 
     name=${repo_name%-}
     if [ "$name" != "$repo_name" ]; then
-        echo "Info: the robot name($repo_name) ends with '-', and will be changed to $name."
+        log "Info: the robot name($repo_name) ends with '-', and will be changed to $name."
         repo_name=$name
     fi
 
@@ -53,19 +58,19 @@ check_robot_name() {
 
     name=${name#$prefix}
     if [ -z "$name" ]; then
-        echo "Error: there is not real robot name. The '$prefix_of_robot_name' is the name prefix."
+        log "Error: there is not real robot name. The '$prefix_of_robot_name' is the name prefix."
         return 1
     fi
 
     local s=${name/robot/-}
     if [ "$s" != "$name" ]; then
-        echo "Error: the robot name can't include reserved word 'robot'."
+        log "Error: the robot name can't include reserved word 'robot'."
         return 1
     fi
 
     local s=${name/gitee/-}
     if [ "$s" != "$name" ]; then
-        echo "Error: the robot name can't include reserved word 'gitee'."
+        log "Error: the robot name can't include reserved word 'gitee'."
         return 1
     fi
 
@@ -73,20 +78,21 @@ check_robot_name() {
     if [ "$repo_name" = "$name" ]; then
         repo_name="${prefix_of_robot_name}-${repo_name}"
 
-        echo "Info: the robot name should have prefix of '$prefix_of_robot_name', and will be changed to $repo_name."
+        log "Info: the robot name should have prefix of '$prefix_of_robot_name', and will be changed to $repo_name."
     fi
 }
 
 build(){
-    local repo_dir=$1
-    local repo_path=$2
+    local robot_name=$1
+    local repo_dir=$2
+    local remote_repo=$3
 
-    check_robot_name $(basename $repo_path)
-    repo_path=$(dirname $repo_path)/$repo_name
+    check_robot_name $robot_name
+    robot_name=$repo_name
 
-    repo_dir=$repo_dir/$repo_name
+    repo_dir=$repo_dir/$robot_name
     if [ -d $repo_dir ]; then
-        echo "$repo_dir is exist"
+        log "$repo_dir is exist"
         return 1
     fi
 
@@ -103,13 +109,16 @@ build(){
 
     rm -fr giteeplugin
 
+
+    repo_path=${remote_repo}/$robot_name
+
     git init .
     git remote add origin https://$repo_path
 
     repo_path=${repo_path//\//\\\/}
 
     sed -i -e "s/{PLUGIN_REPO}/${repo_path}/" ./BUILD.bazel
-    sed -i -e "s/{PLUGIN_NAME}/${repo_name}/" ./BUILD.bazel
+    sed -i -e "s/{PLUGIN_NAME}/${robot_name}/" ./BUILD.bazel
 
     sed -i -e "s/{PLUGIN_REPO}/${repo_path}/" ./build.sh
 
@@ -119,14 +128,21 @@ build(){
 
 cmd_help(){
 cat << EOF
-usage: $me dir-of-repo import-path-of-repo.
-for example: $me ./test github.com/exmaple/test 
+
+Usage: $me robot-name dir-of-robot remote-repository-of-robot.
+
+For Example: $me test . github.com/opensourceways
+
+The command above will
+generate codes at current dir with robot name of 'robot-gitee-test' and
+import path of 'github.com/opensourceways/robot-gitee-test'.
+
 EOF
 }
 
-if [ $pn -lt 2 ]; then
+if [ $pn -lt 3 ]; then
     cmd_help
     exit 1
 fi
 
-build $1 $2
+build $1 $2 $3
