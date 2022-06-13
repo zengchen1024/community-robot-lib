1564bddcbfbb33a10b15d92c55daf7db5934614b
zengchen1024 <chenzeng2@huawei.com>
2022-06-13 11:46:32 +0800

    optimize

--- robot-gitee-framework1/dispatcher.go
+++ robot-gitee-framework1/dispatcher.go
@@ -1,6 +1,8 @@
 package framework
 
 import (
+	"io/ioutil"
+	"net/http"
 	"sync"
 
 	sdk "github.com/opensourceways/go-gitee/gitee"
@@ -21,11 +23,11 @@ type dispatcher struct {
 	wg sync.WaitGroup
 }
 
-func (d *dispatcher) Wait() {
+func (d *dispatcher) wait() {
 	d.wg.Wait() // Handle remaining requests
 }
 
-func (d *dispatcher) Dispatch(eventType string, payload []byte, l *logrus.Entry) error {
+func (d *dispatcher) dispatch(eventType string, payload []byte, l *logrus.Entry) error {
 	switch eventType {
 	case sdk.EventTypeNote:
 		if d.h.noteEventHandler == nil {
@@ -148,4 +150,55 @@ func (d *dispatcher) handleNoteEvent(e *sdk.NoteEvent, l *logrus.Entry) {
 	} else {
 		l.Info()
 	}
+}
+
+func (d *dispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	eventType, eventGUID, payload, ok := parseRequest(w, r)
+	if !ok {
+		return
+	}
+
+	l := logrus.WithFields(
+		logrus.Fields{
+			"event-type": eventType,
+			"event_id":   eventGUID,
+		},
+	)
+
+	if err := d.dispatch(eventType, payload, l); err != nil {
+		l.WithError(err).Error()
+	}
+}
+
+func parseRequest(w http.ResponseWriter, r *http.Request) (eventType string, uuid string, payload []byte, ok bool) {
+	defer r.Body.Close()
+
+	resp := func(code int, msg string) {
+		http.Error(w, msg, code)
+	}
+
+	if r.Header.Get("User-Agent") != "Robot-Gitee-Access" {
+		resp(http.StatusBadRequest, "400 Bad Request: unknown User-Agent Header")
+		return
+	}
+
+	if eventType = r.Header.Get("X-Gitee-Event"); eventType == "" {
+		resp(http.StatusBadRequest, "400 Bad Request: Missing X-Gitee-Event Header")
+		return
+	}
+
+	if uuid = r.Header.Get("X-Gitee-Timestamp"); uuid == "" {
+		resp(http.StatusBadRequest, "400 Bad Request: Missing X-Gitee-Timestamp Header")
+		return
+	}
+
+	v, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		resp(http.StatusInternalServerError, "500 Internal Server Error: Failed to read request body")
+		return
+	}
+	payload = v
+	ok = true
+
+	return
 }
--- robot-gitee-framework1/service.go
+++ robot-gitee-framework1/service.go
@@ -1,11 +1,8 @@
 package framework
 
 import (
-	"io/ioutil"
 	"net/http"
 	"strconv"
-
-	"github.com/sirupsen/logrus"
 
 	"github.com/opensourceways/community-robot-lib/interrupts"
 	"github.com/opensourceways/community-robot-lib/options"
@@ -21,74 +18,27 @@ type Service interface {
 }
 
 func NewService() Service {
-	return &handlers{}
+	return &service{}
 }
 
-func (h *handlers) Run(o options.ServiceOptions) {
-	d := &dispatcher{h: *h}
+type service struct {
+	handlers
+}
+
+func (s *service) Run(o options.ServiceOptions) {
+	d := dispatcher{h: s.handlers}
 
 	defer interrupts.WaitForGracefulShutdown()
 
 	interrupts.OnInterrupt(func() {
-		d.Wait()
+		d.wait()
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 
-	http.Handle("/gitee-hook", d)
+	http.Handle("/gitee-hook", &d)
 
 	httpServer := &http.Server{Addr: ":" + strconv.Itoa(o.Port)}
 
 	interrupts.ListenAndServe(httpServer, o.GracePeriod)
-}
-
-func (d *dispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	eventType, eventGUID, payload, ok := parseRequest(w, r)
-	if !ok {
-		return
-	}
-
-	l := logrus.WithFields(
-		logrus.Fields{
-			"event-type": eventType,
-			"event_id":   eventGUID,
-		},
-	)
-
-	if err := d.Dispatch(eventType, payload, l); err != nil {
-		l.WithError(err).Error()
-	}
-}
-
-func parseRequest(w http.ResponseWriter, r *http.Request) (eventType string, uuid string, payload []byte, ok bool) {
-	defer r.Body.Close()
-
-	resp := func(code int, msg string) {
-		http.Error(w, msg, code)
-	}
-
-	if r.Header.Get("User-Agent") != "Robot-Gitee-Access" {
-		resp(http.StatusBadRequest, "400 Bad Request: unknown User-Agent Header")
-		return
-	}
-
-	if eventType = r.Header.Get("X-Gitee-Event"); eventType == "" {
-		resp(http.StatusBadRequest, "400 Bad Request: Missing X-Gitee-Event Header")
-		return
-	}
-
-	if uuid = r.Header.Get("X-Gitee-Timestamp"); uuid == "" {
-		resp(http.StatusBadRequest, "400 Bad Request: Missing X-Gitee-Timestamp Header")
-		return
-	}
-
-	v, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		resp(http.StatusInternalServerError, "500 Internal Server Error: Failed to read request body")
-		return
-	}
-	payload = v
-	ok = true
-
-	return
 }
