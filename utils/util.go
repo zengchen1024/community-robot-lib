58a325ed67140bc9fa8db04d47caff0b2e0e0e5c
zengchen1024 <chenzeng2@huawei.com>
2022-09-06 14:37:09 +0800

    add load yaml util

--- utils/util.go
+++ utils/util.go
@@ -1,9 +1,26 @@
 package utils
 
-import "regexp"
+import (
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	"sigs.k8s.io/yaml"
+)
 
 var emailRe = regexp.MustCompile(`^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})$`)
 
 func IsValidEmail(email string) bool {
 	return emailRe.MatchString(email)
+}
+
+func LoadFromYaml(path string, cfg interface{}) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	content := []byte(os.ExpandEnv(string(b)))
+
+	return yaml.Unmarshal(content, cfg)
 }
