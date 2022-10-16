0602b5e39d95f062bf6b7813ff4464b201915bfd
zengchen1024 <chenzeng2@huawei.com>
2022-10-16 08:12:37 +0800

    download  method

--- utils/httpclient.go
+++ utils/httpclient.go
@@ -47,6 +47,29 @@ func (hc *HttpClient) ForwardTo(req *http.Request, jsonResp interface{}) (status
 	return
 }
 
+func (hc *HttpClient) Download(req *http.Request) (r []byte, statusCode int, err error) {
+	resp, err := hc.do(req)
+	if err != nil || resp == nil {
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if code := resp.StatusCode; code < 200 || code > 299 {
+		statusCode = code
+
+		var rb []byte
+		if rb, err = ioutil.ReadAll(resp.Body); err == nil {
+			err = fmt.Errorf("response has status:%s and body:%q", resp.Status, rb)
+		}
+
+	} else {
+		r, err = ioutil.ReadAll(resp.Body)
+	}
+
+	return
+}
+
 func (hc *HttpClient) do(req *http.Request) (resp *http.Response, err error) {
 	if resp, err = hc.Client.Do(req); err == nil {
 		return
